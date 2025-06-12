98fa0e6c62d7b1ce92de4196fb9e211219af3f9b
Raquel <106412297+raquelarias123@users.noreply.github.com>
2025-06-11 19:56:49 -0700

    Update JackInTheBox.java

--- src/_04_gui_from_scratch/_2_jack_in_the_box/JackInTheBox.java
+++ src/_04_gui_from_scratch/_2_jack_in_the_box/JackInTheBox.java
@@ -5,17 +5,50 @@ import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
 public class JackInTheBox {
 	JFrame frame = new JFrame();
 	JButton button = new JButton("Suprise!");
+	JLabel imageLabel = new JLabel();
 	
 	public void showButton() {
 		System.out.println("Button clicked");
 		frame.setVisible(true);
 		frame.add(button);
 		frame.pack();
+	}
+	
+	private void showPicture(String fileName) {
+	     try {
+	          JLabel imageLabel = createLabelImage(fileName);
+	          JFrame frame = new JFrame();
+	          frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	          frame.add(imageLabel);
+	          frame.setVisible(true);
+	          frame.pack();
+	     } catch (Exception e) {
+	          e.printStackTrace();
+	     }
+	}
+
+
+	private JLabel createLabelImage(String fileName) {
+	     try {
+	          URL imageURL = getClass().getResource(fileName);
+	          if (imageURL == null) {
+	               System.err.println("Could not find image " + fileName);
+	               return new JLabel();
+	          } else {
+	               Icon icon = new ImageIcon(imageURL);
+	               JLabel imageLabel = new JLabel(icon);
+	               return imageLabel;
+	          }
+	     } catch (Exception e) {
+	          System.err.println("Could not find image " + fileName);
+	          return new JLabel();
+	     }
 	}
 
 	
